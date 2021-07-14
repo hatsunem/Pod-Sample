3a81e52f47b2bf902a899ae3bc5bcda2dd6ff5e2
hatsunem <hatsune_mineta@voyagegroup.com>
2021-07-14 12:26:54 +0900

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  
+//
+//  Copyright (C) 2021 Zucks, Inc.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
